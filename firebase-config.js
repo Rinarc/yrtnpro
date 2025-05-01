a5209d73605a65cc5f433b22f9e3ca618e408cbb
Rinarc <3589297694@qq.com>
2025-05-01 20:23:12 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,12 @@
+// 初始化配置（使用你的实际配置）
 const firebaseConfig = {
   apiKey: "AIzaSyDjrGup3nSXN5SHdG8w0lRHQZ5OmLrQCtQ",
   authDomain: "yrtnpro.firebaseapp.com",
   projectId: "yrtnpro",
-  storageBucket: "yrtn-news.appspot.com",
+  storageBucket: "yrtnpro.firebasestorage.app",
   messagingSenderId: "918761047660",
   appId: "1:918761047660:web:1b963d7cae358e30237a49"
 };
 
 // 初始化Firebase
 firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
